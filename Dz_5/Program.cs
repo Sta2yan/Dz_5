f904a936fdbcfffb206d5f6671b329015298e8b7
Sta2yan <s2_stas@mail.ru>
2022-02-26 20:28:57 +0300

    Добавьте файлы проекта.

---
+++ Dz_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleInQueue;
+            int timeOfReceipt = 10;
+            int hoursInQueueLeft;
+            int minutesInQueueLeft;
+            int minutesInHour = 60;
+
+            Console.Write("Введите кол-во людей в очереди: ");
+            peopleInQueue = Convert.ToInt32(Console.ReadLine());
+
+            hoursInQueueLeft = peopleInQueue * timeOfReceipt / minutesInHour;
+            minutesInQueueLeft = peopleInQueue * timeOfReceipt % minutesInHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursInQueueLeft} часа и {minutesInQueueLeft} минут");
+        }
+    }
+}
